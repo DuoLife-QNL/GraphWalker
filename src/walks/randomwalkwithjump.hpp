d501416ba34319bdee1865efef163d2fbe8e3dc3
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-12-05 21:01:08 +0800

    iteration-sync: update each walk by only one step in each iteration

--- src/walks/randomwalkwithjump.hpp
+++ src/walks/randomwalkwithjump.hpp
@@ -34,19 +34,19 @@ public:
         vid_t dstId = walk_manager.getCurrentId(nowWalk) + blocks[exec_block];
         hid_t hop = walk_manager.getHop(nowWalk);
         unsigned seed = (unsigned)(walkid+dstId+hop+(unsigned)time(NULL));
-        while (dstId >= blocks[exec_block] && dstId < blocks[exec_block+1] && hop < L ){
-            updateInfo(sourId, dstId, threadid, hop);
-            vid_t dstIdp = dstId - blocks[exec_block];
-            eid_t outd = beg_pos[dstIdp+1] - beg_pos[dstIdp];
-            if (outd > 0){
-                eid_t pos = beg_pos[dstIdp] - beg_pos[0] + ((eid_t)rand_r(&seed))%outd;
-                dstId = csr[pos];
-            }else{
-                dstId = rand_r(&seed) % N;
-            }
-            hop++;
-            nowWalk++;
+
+        updateInfo(sourId, dstId, threadid, hop);
+        vid_t dstIdp = dstId - blocks[exec_block];
+        eid_t outd = beg_pos[dstIdp+1] - beg_pos[dstIdp];
+        if (outd > 0){
+            eid_t pos = beg_pos[dstIdp] - beg_pos[0] + ((eid_t)rand_r(&seed))%outd;
+            dstId = csr[pos];
+        }else{
+            dstId = rand_r(&seed) % N;
         }
+        hop++;
+        nowWalk++;
+
         if( hop < L ){
             bid_t p = getblock( dstId );
             if(p>=nblocks) return;
